f126e1e8882c88f862d0086dc39fddefa7fa9bfe
Andrew Plotner <bluebandit21@gmail.com>
2024-12-06 16:36:15 -0500

    Add clean_rebuild_me.sh

---
+++ clean_rebuild_me.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curr_dir=`pwd`
+script_dir=$(dirname $(readlink -f "${0}"))
+cd ${script_dir}
+
+set -e
+
+#cp -r "${script_dir}/../ltp_workspace/ltp_install/" "${script_dir}/build-scripts/config/busybox/initramfs.files.d/"
+
+export CFLAGS="-Wno-dangling-pointer"
+./build-scripts/build-all.sh -p fvp -f busybox all
+
+
+echo "TODO..."
+# Build only the Linux kernel
+
+cd ${curr_dir}
