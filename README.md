7794a211f18d291ba65a31c66eeb5e8d1a0b7f9e
Andrew Plotner <bluebandit21@gmail.com>
2024-12-07 18:58:06 -0500

    Update

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Modified stuff:
 
 ~~(indirect) ```build_scripts/config/busybox/initramfs.files.d/```~~
 ~~Added (automatically, as part of rebuild_me.sh) ltp install files into it, so they'll be present after booting busybox~~
+
+
+
+build-scripts/build-linux.sh
+
+Added following configuration entry:
+        #Andrew new
+        --enable FTRACE
--- clean_rebuild_me.sh
+++ clean_rebuild_me.sh
@@ -9,10 +9,9 @@ set -e
 #cp -r "${script_dir}/../ltp_workspace/ltp_install/" "${script_dir}/build-scripts/config/busybox/initramfs.files.d/"
 
 export CFLAGS="-Wno-dangling-pointer"
+./build-scripts/build-all.sh -p fvp -f busybox clean
 ./build-scripts/build-all.sh -p fvp -f busybox all
 
-
-echo "TODO..."
 # Build only the Linux kernel
 
 cd ${curr_dir}
